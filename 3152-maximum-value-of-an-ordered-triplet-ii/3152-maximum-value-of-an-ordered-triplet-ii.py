6482b15771ae65811b89b330ded4f2d12b42d19e
JJumanah <jjumanah31@gmail.com>
2025-08-26 16:21:39 +0530

    Time: 171 ms (81.68%), Space: 29.5 MB (76.61%) - LeetHub

--- 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.py
+++ 3152-maximum-value-of-an-ordered-triplet-ii/3152-maximum-value-of-an-ordered-triplet-ii.py
@@ -13,9 +13,9 @@ class Solution:
         ans=0
         max_diff=0  
         max_num=0
-        for num in nums:
-            ans=max(ans,max_diff*num)
-            max_diff=max(max_diff,max_num-num)
-            max_num=max(max_num,num)
+        for num in range(len(nums)):
+            ans=max(ans,max_diff*nums[num])
+            max_diff=max(max_diff,max_num-nums[num])
+            max_num=max(max_num,nums[num])
         return ans
 
