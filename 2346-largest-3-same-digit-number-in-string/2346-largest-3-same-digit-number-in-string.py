ea54680f255a62483e1af1977de01e36c97b4986
JJumanah <jjumanah31@gmail.com>
2025-08-26 15:36:34 +0530

    Time: 3 ms (62.05%), Space: 17.8 MB (37.47%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.py
@@ -4,8 +4,7 @@ class Solution:
         for i in range(len(num)-2):
             if num[i]==num[i+1]==num[i+2]:
                 ans=num[i:i+3]
-                res=max(res,ans)
-                
+                res=max(res,ans)             
         return res
 
         
