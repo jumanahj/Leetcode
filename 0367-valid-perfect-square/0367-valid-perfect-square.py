c7fd052510e5eee219416bb64756f5a55544d1ea
JJumanah <jjumanah31@gmail.com>
2025-09-03 10:41:32 +0530

    Time: 0 ms (100%), Space: 12.5 MB (51.88%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -4,8 +4,14 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
+        '''
         n=int(sqrt(num))
         if n*n == num:
             return True 
         return False 
+        '''
+        n=num
+        while n*n > num:
+            n=(n+num//n)//2
+        return n*n == num
         
