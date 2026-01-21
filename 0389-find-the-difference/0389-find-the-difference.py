1744829db2c587e27c91fb1fac4a9f56ed326858
JJumanah <jjumanah31@gmail.com>
2026-01-21 21:44:37 +0530

    Time: 1 ms (78.59%), Space: 12.6 MB (1.86%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,10 +1,21 @@
 class Solution(object):
     def findTheDifference(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+        
+        for ch in t:
+            if t.count(ch) > s.count(ch):
+                return ch
+        
+
+        ''' 
+        res=0
+        for ch in s+t:
+            res ^= ord(ch)
+        return chr(res)
+
+        --- using bitwise operation - very fast and interview expected 
+        '''
+
+        '''
         sl=list(s)
         tl=list(t)
 
@@ -14,13 +25,5 @@ class Solution(object):
             else:
                 sl.remove(i)
         return ""
-
-        ''' 
-        res=0
-        for ch in s+t:
-            res ^= ord(ch)
-        return chr(res)
-
-        --- using bitwise operation - very fast and interview expected 
         '''
         
